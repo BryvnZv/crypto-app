a298888cb03c28a6d976d522513c944d6a6fe6e6
BryvnZv <bryvnzv@gmail.com>
2023-03-05 22:15:55 -0500

    added all dependencies and structured the basic components of the app

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,28 @@
+import React, { useState, useEffect } from "react";
+import { Button, Menu, Typography, Avatar } from "antd";
+import { Link } from "react-router-dom";
+import {
+  HomeOutlined,
+  MoneyCollectOutlined,
+  BulbOutlined,
+  FundOutlined,
+  MenuOutlined,
+} from "@ant-design/icons";
+
+import icon from "../images/cryptocurrency.png";
+
+const Navbar = () => {
+  return (
+    <div className="nav-container">
+      <div className="logo-container">
+        <Avatar src={icon} size="Large" />
+        <Typography.Title level={2} className="logo">
+          <Link to="/">Cryptoverse</Link>
+        </Typography.Title>
+        {/* <Button className="menu-control-container"></Button> */}
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
