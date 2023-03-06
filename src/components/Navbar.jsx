c6a1106084e839e54f1318716b51fca00c6e30be
BryvnZv <bryvnzv@gmail.com>
2023-03-06 09:27:50 -0500

    added in App.css file along with styling and responsive styling for the app

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -19,7 +19,6 @@ const Navbar = () => {
         <Typography.Title level={2} className="logo">
           <Link to="/">Cryptoverse</Link>
         </Typography.Title>
-        {/* <Button className="menu-control-container"></Button> */}
       </div>
     </div>
   );
