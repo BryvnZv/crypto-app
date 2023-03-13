92be4ab498626f02d07479806675dc3daf74060b
BryvnZv <bryvnzv@gmail.com>
2023-03-13 14:10:04 -0400

    minor bug fixes along with a few adjustments. All donw for now !Enjoy

--- src/components/CryptoDetails.jsx
+++ src/components/CryptoDetails.jsx
@@ -89,13 +89,13 @@ const CryptoDetails = () => {
       ),
       icon: <ExclamationCircleOutlined />,
     },
-    {
-      title: "Total Supply",
-      value: `$ ${
-        cryptoDetails?.supply?.total && millify(cryptoDetails?.supply?.total)
-      }`,
-      icon: <ExclamationCircleOutlined />,
-    },
+    // {
+    //   title: "Total Supply",
+    //   value: `$ ${
+    //     cryptoDetails?.supply?.total && millify(cryptoDetails?.supply?.total)
+    //   }`,
+    //   icon: <ExclamationCircleOutlined />,
+    // },
     {
       title: "Circulating Supply",
       value: `$ ${
@@ -127,7 +127,11 @@ const CryptoDetails = () => {
           <Option key={date}>{date}</Option>
         ))}
       </Select>
-      {/* line chart  */}
+      {/* <LineChart
+        coinHistory={coinHistory}
+        currentPrice={millify(cryptoDetails?.price)}
+        coinName={cryptoDetails?.name}
+      /> */}
       <Col className="stats-container">
         <Col className="coin-value-statistics">
           <Col className="coin-value-statistics-heading">
@@ -172,7 +176,7 @@ const CryptoDetails = () => {
           <Title level={3} className="coin-details-heading">
             What is {cryptoDetails.name}?
           </Title>
-          {HTMLReactParser(cryptoDetails.description)}
+          {HTMLReactParser(cryptoDetails?.description)}
         </Row>
         <Col className="coin-links">
           <Title level={3} className="coin-details-heading">
--- src/components/LineChart.jsx
+++ src/components/LineChart.jsx
@@ -10,13 +10,11 @@ const LineChart = ({ coinHistory, currentPrice, coinName }) => {
 
   for (let i = 0; i < coinHistory?.data?.history?.length; i += 1) {
     coinPrice.push(coinHistory?.data?.history[i].price);
-  }
-
-  for (let i = 0; i < coinHistory?.data?.history?.length; i += 1) {
     coinTimestamp.push(
       new Date(coinHistory?.data?.history[i].timestamp).toLocaleDateString()
     );
   }
+
   const data = {
     labels: coinTimestamp,
     datasets: [
